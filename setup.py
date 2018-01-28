730564873dbd4431cddc18b22d6f649002425cc0
Vaishali Garg <vaishaligarg@gmail.com>
2018-01-27 20:10:41 -0800

    Tweet version2

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='tweets-dl',
+    version='0.1.2',
+    author='Vaishali Garg',
+    author_email='vaishaligarg@gmail.com',
+    url='https://github.com/Vaishali-Garg/tweet-downloader',
+    packages=['tweet_downloader'],
+    include_package_data=True,
+    install_requires=[
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        tweets-dl=tweet_downloader.tweet_dl:main
+    ''',
+)
